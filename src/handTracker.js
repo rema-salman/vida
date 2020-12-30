99426808e7217c5a8562f39404ddd71f7b0cd53e
rema-salman <rema.salman@outlook.com>
2020-12-30 20:31:06 +0100

    adding audio with 2 controllers, making 1 audio as a base and another dynamically changing

--- src/handTracker.js
+++ src/handTracker.js
@@ -15,9 +15,12 @@ navigator.getUserMedia =
 const video = document.querySelector("#video");
 const canvas = document.querySelector("#canvas");
 const audio = document.querySelector("#audio");
+const baseAudio = document.querySelector("#base-audio");
 const context = canvas.getContext("2d");
 let model;
 
+//set the volume of the base sound to be lower than the rest
+baseAudio.volume = 0.2;
 // Fetch an array of devices of a certain type (can be cameras, microphones, and headphones)
 async function getConnectedDevices(type) {
   const devices = await navigator.mediaDevices.enumerateDevices();
@@ -30,7 +33,7 @@ const cameras = getConnectedDevices("videoinput");
 //promise function stream the video
 function startUserMedia(stream) {
   video.srcObject = stream;
-  setInterval(runDetection, 1000);
+  setInterval(runDetection, 2000);
 }
 
 //main hand tracker video
@@ -50,10 +53,16 @@ handTrack.startVideo(video).then((status) => {
   }
 });
 
+// Load the model.
+handTrack.load(modelParams).then((lmodel) => {
+  model = lmodel;
+});
+
 // detect objects in the video.
 function runDetection() {
   model.detect(video).then((predictions) => {
     console.log("Predictions: ", predictions);
+
     //for the blue-box around the hand
     // model.renderPredictions(predictions, canvas, context, video);
     if (predictions.length !== 0) {
@@ -68,21 +77,32 @@ function runDetection() {
       // the hand was detected in teh top-left box
       if (x1 > 300 && y1 < 250) {
         console.log("top-left");
+        audio.src = "./natural-sounds/rainforest_birds.mp3";
       }
       // the hand was detected in teh top-right box
       else if (x1 < 300 && y1 < 250) {
         console.log("top-right");
+        audio.src = "./natural-sounds/night_sounds_and_fire.mp3";
       } // the hand was detected in teh bottom-left box
       else if (x1 > 300 && y1 > 250) {
         console.log("bottom-left");
+        audio.src = "./natural-sounds/snow_steps.mp3";
       } else if (x1 < 300 && y1 > 250) {
         console.log("bottom-right");
-      } else console.log("no area was detected");
+        audio.src = "./natural-sounds/wind_through_trees.mp3";
+      } else
+        console.log("no area was detected, playing sound is waterfall_loop");
     }
   });
 }
 
-// Load the model.
-handTrack.load(modelParams).then((lmodel) => {
-  model = lmodel;
-});
+// If player not supported by other browsers then create
+// buttons for paly/ pause
+// var player = document.getElementById("player");
+
+// player.addEventListener("play", () => {
+//   audio.play();
+// });
+// player.addEventListener("pause", () => {
+//   audio.pause();
+// });
